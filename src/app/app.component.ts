f9312ea50378d715aa065ddfb1f20b82ded71439
suvi2913 <server.visu143@gmail.com>
2023-03-11 23:50:32 +0530

    employee table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project-demo';
+	userList = [
+		{ uid: 101, name : "Rajeev", location : "Dehradun"},
+		{ uid: 102, name : "Raj", location : "Delhi"},
+		{ uid: 103, name : "Raju", location : "Dehradun"},
+		{ uid: 104, name : "Rajni", location : "Mumbai"},
+		{ uid: 105, name : "Rajneesh", location : "Lucknow"}
+	]
+
+	onEdit(){
+		alert("Edit")
+	}
+
+	onDelete(){
+		alert("delete")
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
